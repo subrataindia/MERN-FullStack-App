fed431f155921fa96d33bf12ede03e40eea38962
Subrat Kumar Das <business.ttrc@gmail.com>
2022-05-19 20:59:38 +0530

    add server code

--- sampleapp-backend/src/server.js
+++ sampleapp-backend/src/server.js
@@ -1,7 +1,40 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
+const articleInfo = {
+    'learn-react': {
+        upvotes: 0,
+        comments: [],
+    },
+    'learn-node' : {
+        upvotes: 0,
+        comments: [],
+    },
+    'learn-mongo' : {
+        upvotes: 0,
+        comments: [],
+    }
+}
 
 const app = express();
 
-app.get('/hello', (req, res) => res.send('Hello! World'));
+app.use(bodyParser.json());
 
+app.get('/hello', (req, res) => res.send('Hello! World'));
+app.get('/hello/:name', (req, res) => res.send(`Hello! ${req.params.name}`));
+app.post('/hello', (req, res) => res.send(`Hello! ${req.body.name}!`));
+app.post('/api/articles/:name/upvote', (req, res) => {
+    const articleName = req.params.name;
+
+    articleInfo[articleName].upvotes += 1;
+    res.status(200).send(`${articleName} now has ${articleInfo[articleName].upvotes} upvotes.`)
+})
+app.post('api/articles/:name/add-comment', (req, res) => {
+    const {username, text } = req.body;
+    const articleName = req.params.name;
+
+    articleInfo[articleName].comments.push({username, text});
+
+    res.status(200).send(articleInfo[articleName]);
+})
 app.listen(8000, ()=> console.log('Backend Running on Port 8000'));
